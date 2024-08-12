13a5b2f3be795726884222bc253554ca86836b0b
sobia-r <sobia.rasheed@gmail.com>
2024-08-12 22:18:13 +0400

    Event Listern in JavaScript

---
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -0,0 +1,15 @@
+/*document.querySelector("button").addEventListener("click",handleClicked) //function call with braces means to get it call straightforward rather waiting for button to be clicked
+//that's why we just call the name so it waits for click to happen
+function handleClicked()
+{
+    alert("I got Clicked")
+}*/
+
+
+//transform to anaymous function
+var drumButtonCount =document.querySelectorAll(".drum").length
+for(var i=0;i<drumButtonCount;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){ alert ("i got clicked")})
+}
+
