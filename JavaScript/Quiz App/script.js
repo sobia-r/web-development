089da7b215e13d0605c79c415eb4d155de810595
sobia-r <sobia.rasheed@gmail.com>
2024-08-27 18:32:41 +0400

    Quiz App using Javascript

---
+++ JavaScript/Quiz App/script.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question: "What is the capital of France?",
+        a: "Berlin",
+        b: "Madrid",
+        c: "Paris",
+        d: "Lisbon",
+        correct: "c"
+    },
+    {
+        question: "Who is the President of the USA?",
+        a: "Donald Trump",
+        b: "Barack Obama",
+        c: "Joe Biden",
+        d: "George Bush",
+        correct: "c"
+    },
+    {
+        question: "What is 2 + 2?",
+        a: "3",
+        b: "4",
+        c: "5",
+        d: "6",
+        correct: "b"
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "HyperText Markup Language",
+        b: "Hyperloop Machine Language",
+        c: "Hypertext Machine Language",
+        d: "HyperText Main Language",
+        correct: "a"
+    },
+   
